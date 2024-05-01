67596e33686929f1c31a7413f31e02223078984d
aditya <adityadewhy@gmail.com>
2024-05-01 10:30:45 +0530

    bg added

--- otp/src/App.jsx
+++ otp/src/App.jsx
@@ -1,10 +1,8 @@
-import {useState} from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Phone from "./pages/Phone";
 import Otp from "./pages/Otp";
 
 export default function App() {
-	const [phone, setPhone] = useState(false);
 
 	return (
 		<div>
--- otp/src/pages/Phone.jsx
+++ otp/src/pages/Phone.jsx
@@ -8,28 +8,28 @@ export default function Phone() {
 		const enteredNum = phoneEl.value;
 		if (enteredNum.length == 10) {
 			navigate("/otp");
-			//console.log("good input"); useNavigate here to teleport to otp page
 		} else {
 			alert("incorrect phone format");
 		}
 	};
 
 	return (
-		<div>
-			<h1>Login via OTP</h1>
+		<div class="grid grid-rows-3 gap-4 place-items-center bg-gray-600">
+				<h1>Login via OTP</h1>
 
-			<input
-				type="tel"
-				id="phone"
-				name="phoneNumber"
-				placeholder="enter you phone number"
-			/>
-			<input
-				type="button"
-				id="phoneSubmitButton"
-				onClick={handleClick}
-				value="submit"
-			/>
-		</div>
+				<input
+					type="tel"
+					id="phone"
+					name="phoneNumber"
+					placeholder="enter you phone number"
+				/>
+				<input
+					type="button"
+					id="phoneSubmitButton"
+					onClick={handleClick}
+					value="submit"
+					class="bg-blue-400"
+				/>
+			</div>
 	);
 }
