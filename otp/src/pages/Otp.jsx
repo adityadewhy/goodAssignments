b81630bf30bf49991ef5f43f85a34610382b2321
aditya <adityadewhy@gmail.com>
2024-05-09 13:03:48 +0530

    just work in otp page now. color issue fixed

--- otp/src/pages/Otp.jsx
+++ otp/src/pages/Otp.jsx
@@ -1,13 +1,35 @@
-export default function Otp(){
-    return(
-        <div class="grid grid-rows-3 gap-4 place-items-center bg-gray-600">
-            <p>enter the otp below</p>
-            <div id="boxes" class="grid grid-cols-4" >
-                <input type="number" id="1" class="m-3 h-5 w-5"/>
-                <input type="number" id="2" class="m-3 h-5 w-5"/>
-                <input type="number" id="3" class="m-3 h-5 w-5"/>
-                <input type="number" id="4" class="m-3 h-5 w-5"/> 
-            </div>
-        </div>
-    )
+export default function Otp() {
+	return (
+		<div class="grid grid-rows-3 gap-4 place-items-center bg-gray-600">
+			<p>enter the otp below</p>
+			<div id="boxes" class="grid grid-cols-4">
+				<input
+					type="text"
+					id="1"
+					class="m-3 h-10 w-10 overflow-hidden color-black text-center"
+				/>
+				<input
+					type="text"
+					id="2"
+					class="m-3 h-10 w-10 overflow-hidden color-black text-center"
+				/>
+				<input
+					type="text"
+					id="3"
+					class="m-3 h-10 w-10 overflow-hidden color-black text-center"
+				/>
+				<input
+					type="text"
+					id="4"
+					class="m-3 h-10 w-10 overflow-hidden color-black text-center"
+				/>
+			</div>
+			<button
+				id="otpSubmitButton"
+				onClick={handleClick}
+				value="submit otp"
+				class="bg-blue-400 text-white rounded-full p-2 m-2"
+			></button>
+		</div>
+	);
 }
--- otp/src/pages/Phone.jsx
+++ otp/src/pages/Phone.jsx
@@ -15,22 +15,23 @@ export default function Phone() {
 
 	return (
 		<div class="grid grid-rows-3 gap-4 place-items-center bg-gray-600">
-				<h1>Login via OTP</h1>
+			<h1 class="text-white">Login via OTP</h1>
 
-				<input
-					type="tel"
-					id="phone"
-					name="phoneNumber"
-					placeholder="enter you phone number"
-					//fix text color
-				/>
-				<input
-					type="button"
-					id="phoneSubmitButton"
-					onClick={handleClick}
-					value="submit"
-					class="bg-blue-400"
-				/>
-			</div>
+			<input
+				type="tel"
+				id="phone"
+				name="phoneNumber"
+				placeholder="enter you phone number"
+				class="placeholder: text-center"
+			/>
+			<button
+				id="phoneSubmitButton"
+				onClick={handleClick}
+				value="submitPhone"
+				class="bg-blue-400 text-white rounded-full p-2 m-2"
+			>
+				Submit Phone
+			</button>
+		</div>
 	);
 }
