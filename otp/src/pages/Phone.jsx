7c493aad7e5be64dd7fa7cc1509d9a04007f6d4d
aditya <adityadewhy@gmail.com>
2024-05-12 18:46:45 +0530

    all set

--- otp/src/pages/Phone.jsx
+++ otp/src/pages/Phone.jsx
@@ -14,21 +14,21 @@ export default function Phone() {
 	};
 
 	return (
-		<div class="grid grid-rows-3 gap-4 place-items-center bg-gray-600">
-			<h1 class="text-white">Login via OTP</h1>
+		<div className="bg-gray-600 grid grid-cols-1 place-items-center gap-3 m-3 p-3">
+			<h1 className="text-white p-3">Login via OTP</h1>
 
 			<input
 				type="tel"
 				id="phone"
 				name="phoneNumber"
 				placeholder="enter you phone number"
-				class="placeholder: text-center"
+				className="placeholder: text-center"
 			/>
 			<button
 				id="phoneSubmitButton"
 				onClick={handleClick}
 				value="submitPhone"
-				class="bg-blue-400 text-white rounded-full p-2 m-2"
+				className="bg-blue-400 text-white rounded-full p-2 m-2"
 			>
 				Send OTP
 			</button>
